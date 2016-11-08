c0b1936aae38be813fcf7a0357c983e60a368776
zhizuqiu <994490929@qq.com>
2016-11-08 14:30:01 +0800

    delete some

--- dist/server.js
+++ dist/server.js
@@ -7,11 +7,11 @@ var server_port = 8002;
 /////////////////////////////
 
 var WebSocket = require('ws');
+var uuid = require('node-uuid');
 var WebSocketServer = WebSocket.Server,
     wss = new WebSocketServer({ port: server_port });
-var uuid = require('node-uuid');
 var clients = [];
-function wsSend(type, client_uuid, data) {
+function wsSend(type, data) {
     for (var i = 0; i < clients.length; i++) {
         var clientSocket = clients[i].ws;
         if (clientSocket.readyState === WebSocket.OPEN&&data['title']==clients[i].page) {
@@ -19,23 +19,20 @@ function wsSend(type, client_uuid, data) {
             if(data['danmu']){
                 clientSocket.send(JSON.stringify({
                     "type": type,
-                    "message": data['danmu'],
-                    "time": time,
+                    "message": data['danmu']
                 }));
             }
         }
     }
 }
-var clientIndex = 1;
 wss.on('connection', function(ws) {
-    var client_uuid = uuid.v4();
+    var client_uuid = uuid.v4();//use for closeSocket()
     ws.on('message', function(message) {
         var data = JSON.parse(message);
         if(data['type']=='login'){
-            clientIndex += 1;
             clients.push({ "id": client_uuid , "ws": ws , "page":data['title']});
         }
-        wsSend("message", client_uuid, data);
+        wsSend("message", data);
     });
     var closeSocket = function(customMessage) {
         for (var i = 0; i < clients.length; i++) {
@@ -47,16 +44,10 @@ wss.on('connection', function(ws) {
                     disconnect_message = "disconnected";
                 }
                 clients.splice(i, 1);
-                wsSend("logout", client_uuid, disconnect_message);
             }
         }
     };
     ws.on('close', function () {
         closeSocket();
-    });
-    process.on('SIGINT', function () {
-        console.log("Closing things");
-        closeSocket('Server has disconnected');
-        process.exit();
     });
 });
