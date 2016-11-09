7d7a37505957a0854864b125e0e033b7c5a0de55
zhizuqiu <994490929@qq.com>
2016-11-09 09:32:57 +0800

    修改原生弹幕的样式

--- example/client/danmu-comment.js
+++ example/client/danmu-comment.js
@@ -8,6 +8,7 @@
  * danmu_speed : danmu's speed
  *  keycode : keycode to show the input modal , such :  13 enter , 9 Tab , 36 Home
  *  danmu_style : 0 right to left , 1 tip
+ *  danmu_style0_css : set style of font for danmu_style0
  */
 
 //////////////////////////////////////
@@ -16,12 +17,11 @@ var server_port = "8002";
 var danmu_speed = 1;
 var keycode = '13';
 var danmu_style = 0;
+var danmu_style0_css = "font-family: SimHei; font-weight: 900;  font-size: larger;  color: rgb(102, 102, 102);  text-shadow: 1px 1px rgba(153,153,153, 0.8);";
 //////////////////////////////////////
 
 window.onload=function(){
-    if(!danmu_style){
-        moveDanmu();
-    }
+    moveDanmu();
     addInput();
     bindWebsocket();
 }
@@ -89,13 +89,13 @@ function addInput(){
     var div1_3 = document.createElement("div");
     div1_3.setAttribute("style", "text-align: right;");
     var button = document.createElement("button");
-    button.setAttribute("onclick", "if(socket_danmu) socket_danmu.send(JSON.stringify({'type':'mess','title':document.title,'danmu':document.getElementById('insert_danmu').value}))");
+    button.setAttribute("onclick", "if(socket_danmu) socket_danmu.send(JSON.stringify({'type':'mess','title':document.title,'danmu':document.getElementById('insert_danmu').value})); document.getElementById('alert_danmu').style.display='none';document.getElementById('danmu_shadow').style.display='none';");
     button.setAttribute("style", "height: 28px;   margin: 0 6px;  padding: 5px 10px;  border: 1px solid #dedede;  background-color: #f1f1f1;  -moz-border-radius: 5px;  -webkit-border-radius: 5px;  border-radius: 5px;  cursor: pointer;border-color: #4898d5;  background-color: #2e8ded;  color: #fff;");
     button.innerHTML = "submit";
     div1_3.appendChild(button);
     if(danmu_style==1){
         var button_remove = document.createElement("button");
-        button_remove.setAttribute("onclick", "clear_danmu();");
+        button_remove.setAttribute("onclick", "clear_danmu();document.getElementById('alert_danmu').style.display='none';document.getElementById('danmu_shadow').style.display='none';");
         button_remove.setAttribute("style", "height: 28px;    margin: 0 6px;  padding: 5px 10px;  border: 1px solid #dedede;  background-color: #f1f1f1;  -moz-border-radius: 5px;  -webkit-border-radius: 5px;  border-radius: 5px;  cursor: pointer;background:#C9C5C5;  color: #fff");
         button_remove.innerHTML = "remove";
         div1_3.appendChild(button_remove);
@@ -118,7 +118,7 @@ function addDanmu_style_0(danmu) {
     var parent = document.getElementsByTagName("body")[0];
     var div = document.createElement("div");
     div.setAttribute("name", "danmu");
-    div.setAttribute("style", "position:fixed; white-space:nowrap; left:"+w+"; top:"+h+";");
+    div.setAttribute("style", "position:fixed; white-space:nowrap; left:"+w+"; top:"+h+";"+danmu_style0_css);
     div.innerHTML = danmu;
     document.getElementById('insert_danmu').value = "";
     parent.appendChild(div);
@@ -173,8 +173,9 @@ function fadeIn(el,time){
 function clear_danmu(){
     var count = alerts.length;
     for(var i =0 ;i<alerts.length;i++){
-        var parent = document.getElementsByTagName("body")[0];
-        parent.removeChild(document.getElementById(alerts[i].id));
+        //var parent = document.getElementsByTagName("body")[0];
+        //parent.removeChild(document.getElementById(alerts[i].id));
+        document.getElementById(alerts[i].id).setAttribute("name","danmu");
     }
     alerts.splice(0, count);
 }
